7c0790c20f29b8b788e036975b4b968a94f16978
RahulThalwal <rahul835090@gmail.com>
2025-07-30 00:21:39 +0530

    Add validation for user data

--- php/user_data.php
+++ php/user_data.php
@@ -6,9 +6,30 @@
     $sql2 = "SELECT  * FROM messages WHERE (incoming_msg_id = {$row['user_id']} OR  outgoing_msg_id = {$row[$user_id]})
     AND (outgoing_msg_id = {$outgoing_id} OR incoming_msg_id = {$outgoing_id}) ORDER BY  msg_id DESC LIMIT 1";
 
- $query =  mysqli_query($conn, $sql2);
+ $query2 =  mysqli_query($conn, $sql2);
+ $row2 = mysqli_fetch_assoc($query2);
 
+ 
 
+ (mysqli_num_rows($query2) > 0 ) ? $result = $row['msg'] : $result ["No message available"];
+// if user hasn't chat each others
+ (strlen($result) > 28) ? $msg = substr($result, 0, 28 ) . '.....' : $msg = $result;
+ // if user chat is grater than 20
+
+ if(mysqli_num_rows($query2) != 0){
+    if($row2 ['msg'] == '' && $row2['msg_img'] != ''){ // if user cht is image
+        $img = 'image'; 
+    }
+ }
+
+ if(isset($row2['outgoing_msg_id'])){
+    ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: ": $you = '';
+
+ }else {
+    $you = '';
+ }
+
+//  (row['status'] == "offilne_now")? $offline = 
 
 
     $output .=    '<a href="chat.php?user_id=' . $row['user_id'] . '">
